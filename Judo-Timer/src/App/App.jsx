df984a832317beb58016c3a65de511a5f400da0e
Эдуард Булак <bulakea@mail.ru>
2023-04-26 00:00:30 +0300

    feat: add structure for workplace

---
+++ Judo-Timer/src/App/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css';
+import Timer from '../Timer/Timer';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className='Judo-container'>
+      <Timer/>
+    </div>
+  )
+}
+
+export default App
---
+++ Judo-Timer/src/Timer/Timer.jsx
@@ -0,0 +1,13 @@
+import "./Timer.css"
+
+function Timer() {
+    return (
+      <div className='Timer-container'>
+        <span>00</span>
+        <span>:</span>
+        <span>00</span>
+      </div>
+    )
+  }
+  
+  export default Timer
